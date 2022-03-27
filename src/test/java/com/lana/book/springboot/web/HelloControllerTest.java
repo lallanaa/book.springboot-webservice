c362e2841b9435c3abf24b80a5f207551bd55f07
sungheena <na.sunghee1@gmail.com>
2022-03-28 00:18:14 +0900

    chapter02 테스트 코드 작성

---
+++ src/test/java/com/lana/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package com.lana.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void return_hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void retrun_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))   // param 은 API 테스트 시 요청 파라미터 설정. only String
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))    // jsonPath 는 json 응답값을 필드별로 검증. $ 기준으로 필드명 명시.
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
